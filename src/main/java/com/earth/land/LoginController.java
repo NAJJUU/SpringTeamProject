3b61271cbe495948ddde6c091500e8bf48458704
NAJJUU <wnghk1342@naver.com>
2023-05-25 15:15:14 +0900

    메인 메서드 추가

--- src/main/java/com/earth/land/LoginController.java
+++ src/main/java/com/earth/land/LoginController.java
@@ -2,6 +2,6 @@ package com.earth.land;
 
 public class LoginController {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello Hello Hello");
 	}
 }
